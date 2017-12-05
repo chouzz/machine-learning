279b99454e9a22b68a9818cc4abcecc3ba9a954c
zhouhua <zhouhua25@qq.com>
2017-12-05 16:00:59 +0800

    build a network

--- tensorflow5.py
+++ tensorflow5.py
@@ -9,10 +9,10 @@ import tensorflow as tf
 
 def add_layer(inputs,in_size,out_size,activate_function=None):#定义一个神经层的函数
     Weights = tf.Variable(tf.random_normal([in_size,out_size]))#生成随机矩阵
-    biases = tf.Variable(tf.zeros([1,out_size]+0.1)#类似列表的东西
-    Wx_plus_b = tf.matmul(inputs,Weights)+biases#矩阵乘法，还没激活的值存在这里
+    biases = tf.Variable(tf.zeros([1,out_size])+0.1)#类似列表的东西
+    Wx_plus_b = tf.matmul(inputs,Weights)+biases
     if avtivation_function is None:
         outputs = Wx_plus_b
     else:
         outputs = activate_function(Wx_plus_b)
-    return outputs
+    return outputs#矩阵乘法，还没激活的值存在这里
