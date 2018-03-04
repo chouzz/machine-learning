952f9d9509cec9f264fa5901abba207e9c4664ae
zhouhua <zhouhua25@qq.com>
2018-03-04 21:56:03 +0800

    add practice

---
+++ practice/my_lineRegression.py
@@ -0,0 +1,34 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+np.random.seed(0)
+x = np.linspace(0, 6, 11) + np.random.randn(11)
+x = np.sort(x)
+y = x ** 2 + 2 + np.random.randn(11)
+
+
+def compute_gradient(m_current, b_current, learning_rate):
+    N = len(x)  # 数据的长度
+    m_gradient = 0.0
+    b_gradient = 0.0
+    for i in range(len(x)):
+        m_gradient += -(2 / N) * x[i] * (y[i] - (m_current * x[i] + b_current))
+        b_gradient += -(2 / N) * (y[i] - (m_current * x[i] + b_current))
+
+    new_m = m_current - (learning_rate*m_gradient)
+    new_b = b_current - (learning_rate*b_gradient)
+    return new_m, new_b
+
+def optimizer():
+    w = 0
+    b = 0
+    for i in range(1000):
+        w, b = compute_gradient(w, b, 0.04)
+    y_pre = x * w + b
+    print(w, b)
+    return y_pre
+
+plt.plot(x, y, 'ro')
+plt.plot(x, optimizer(), 'b-')
+
+plt.show()
--- tensorflow6_neural_network.py
+++ tensorflow6_neural_network.py
@@ -8,40 +8,41 @@ Created on Tue Dec  5 14:20:30 2017
 import tensorflow as tf
 import numpy as np
 
-def add_layer(inputs,in_size,out_size,activation_function=None):#定义一个神经层的函数
-    Weights = tf.Variable(tf.random_normal([in_size,out_size]))#生成随机矩阵
-    biases = tf.Variable(tf.zeros([1,out_size])+0.1)#类似列表的东西
-    Wx_plus_b = tf.matmul(inputs,Weights) + biases #矩阵乘法，还没激活的值存在这里
+
+def add_layer(inputs, in_size, out_size, activation_function=None):  # 定义一个神经层的函数
+    Weights = tf.Variable(tf.random_normal([in_size, out_size]))  # 生成随机矩阵
+    biases = tf.Variable(tf.zeros([1, out_size]) + 0.1)  # 类似列表的东西
+    Wx_plus_b = tf.matmul(inputs, Weights) + biases  # 矩阵乘法，还没激活的值存在这里
     if activation_function is None:
-        outputs = Wx_plus_b#线性函数，直接输出
+        outputs = Wx_plus_b  # 线性函数，直接输出
     else:
         outputs = activation_function(Wx_plus_b)
     return outputs
 
-x_data = np.linspace(-1,1,300)[:,np.newaxis]#-1到1之间，维度为300
-noise = np.random.normal(0,0.05,x_data.shape)#方差0.05 ，格式是x_data形式
-y_data = np.square(x_data)-0.5 + noise#加上噪点，不完全是二次函数
 
-xs = tf.placeholder(tf.float32,[None,1])
-ys = tf.placeholder(tf.float32,[None,1])
-#xs ys 要给train_step的值，None是指无论给多少个例子都可以
-l1 = add_layer(xs,1,10,activation_function=tf.nn.relu)
-#输入为x_data，输入层为1，中间层为10，使用通常的激活函数nn.relu激活
-prediction = add_layer(l1,10,1,activation_function=None)
-#输入为中间层的输出，输出为1个神经元，激活函数为线性函数
+x_data = np.linspace(-1, 1, 300)[:, np.newaxis]  # -1到1之间，维度为300
+noise = np.random.normal(0, 0.05, x_data.shape)  # 方差0.05 ，格式是x_data形式
+y_data = np.square(x_data) - 0.5 + noise  # 加上噪点，不完全是二次函数
+
+xs = tf.placeholder(tf.float32, [None, 1])
+ys = tf.placeholder(tf.float32, [None, 1])
+# xs ys 要给train_step的值，None是指无论给多少个例子都可以
+l1 = add_layer(xs, 1, 10, activation_function=tf.nn.relu)
+# 输入为x_data，输入层为1，中间层为10，使用通常的激活函数nn.relu激活
+prediction = add_layer(l1, 10, 1, activation_function=None)
+# 输入为中间层的输出，输出为1个神经元，激活函数为线性函数
 
 loss = tf.reduce_mean(tf.reduce_sum(tf.square(y_data - prediction),
                                     reduction_indices=[1]))
-#square平方，reduce_sum求和，reduce_mean求平均
+# square平方，reduce_sum求和，reduce_mean求平均
 train_step = tf.train.GradientDescentOptimizer(0.3).minimize(loss)
-#训练步骤，使用最常用的优化器，0.1为学习效率，优化器是要使loss最小
+# 训练步骤，使用最常用的优化器，0.1为学习效率，优化器是要使loss最小
 
 init = tf.initialize_all_variables()
 sess = tf.Session()
 sess.run(init)
 
-for i in range(1000):#重复1000次
-    sess.run(train_step,feed_dict={xs:x_data,ys:y_data})
-    if i%50 == 0:
-        print(sess.run(loss,feed_dict={xs:x_data,ys:y_data}))
-             
+for i in range(1000):  # 重复1000次
+    sess.run(train_step, feed_dict={xs: x_data, ys: y_data})
+    if i % 50 == 0:
+        print(sess.run(loss, feed_dict={xs: x_data, ys: y_data}))
