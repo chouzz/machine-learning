6a3a2085aac34a2a4e85dcc8bae427eeb7a41379
zhouhua <zhouhua25@qq.com>
2018-03-28 12:28:30 +0800

    增加sklearn的方法用于线性回归

--- my_lineRegression/lineRegression_usesklearn.py
+++ my_lineRegression/lineRegression_usesklearn.py
@@ -3,7 +3,16 @@ import numpy as np
 from sklearn import datasets, linear_model
 from sklearn.metrics import mean_squared_error, r2_score
 
-# Load the diabetes dataset
-diabetes = datasets.load_diabetes()
+np.random.seed(0)
+x = np.linspace(0, 6, 11) + np.random.randn(11)
+x = np.sort(x)
+y = x ** 2 + 2 + np.random.randn(11)
+x=x[:,np.newaxis]
+print(x)
+regr = linear_model.LinearRegression()
+regr.fit(x,y)
+y_pre = regr.predict(x)
 
-print(diabetes)
+plt.scatter(x,y)
+plt.plot(x,y_pre)
+plt.show()
